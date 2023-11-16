b611f82b834e4411e245a1f10278030fde4843c2
WILDCODER <goodman.ra23@gmail.com>
2023-11-17 00:09:18 +0800

    Added window controls

--- MainScreen.Designer.cs
+++ MainScreen.Designer.cs
@@ -28,44 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.bunifuPictureBox1 = new Bunifu.UI.WinForms.BunifuPictureBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SidePanel = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Header = new System.Windows.Forms.Panel();
+            this.Minimize = new System.Windows.Forms.Label();
+            this.Maximize = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Label();
+            this.MouseDrag = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.bunifuPictureBox1 = new Bunifu.UI.WinForms.BunifuPictureBox();
+            this.SidePanel.SuspendLayout();
+            this.Header.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // SidePanel
             // 
-            this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.bunifuPictureBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(421, 880);
-            this.panel1.TabIndex = 0;
-            // 
-            // bunifuPictureBox1
-            // 
-            this.bunifuPictureBox1.AllowFocused = false;
-            this.bunifuPictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.bunifuPictureBox1.AutoSizeHeight = true;
-            this.bunifuPictureBox1.BorderRadius = 0;
-            this.bunifuPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuPictureBox1.Image")));
-            this.bunifuPictureBox1.IsCircle = false;
-            this.bunifuPictureBox1.Location = new System.Drawing.Point(116, 67);
-            this.bunifuPictureBox1.Name = "bunifuPictureBox1";
-            this.bunifuPictureBox1.Size = new System.Drawing.Size(171, 171);
-            this.bunifuPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.bunifuPictureBox1.TabIndex = 0;
-            this.bunifuPictureBox1.TabStop = false;
-            this.bunifuPictureBox1.Type = Bunifu.UI.WinForms.BunifuPictureBox.Types.Square;
+            this.SidePanel.BackColor = System.Drawing.Color.Black;
+            this.SidePanel.Controls.Add(this.label1);
+            this.SidePanel.Controls.Add(this.bunifuPictureBox1);
+            this.SidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SidePanel.Location = new System.Drawing.Point(0, 44);
+            this.SidePanel.Name = "SidePanel";
+            this.SidePanel.Size = new System.Drawing.Size(421, 836);
+            this.SidePanel.TabIndex = 0;
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Modern No. 20", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
@@ -75,20 +67,103 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Harmony Player";
             // 
+            // Header
+            // 
+            this.Header.BackColor = System.Drawing.Color.Black;
+            this.Header.Controls.Add(this.Minimize);
+            this.Header.Controls.Add(this.Close);
+            this.Header.Controls.Add(this.Maximize);
+            this.Header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Header.Location = new System.Drawing.Point(0, 0);
+            this.Header.Name = "Header";
+            this.Header.Size = new System.Drawing.Size(1520, 44);
+            this.Header.TabIndex = 2;
+            // 
+            // Minimize
+            // 
+            this.Minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Minimize.AutoSize = true;
+            this.Minimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minimize.ForeColor = System.Drawing.Color.White;
+            this.Minimize.Location = new System.Drawing.Point(1361, 0);
+            this.Minimize.Name = "Minimize";
+            this.Minimize.Size = new System.Drawing.Size(48, 42);
+            this.Minimize.TabIndex = 5;
+            this.Minimize.Text = "🗕";
+            this.Minimize.Click += new System.EventHandler(this.Minimize_Click);
+            this.Minimize.MouseEnter += new System.EventHandler(this.Minimize_MouseEnter);
+            this.Minimize.MouseLeave += new System.EventHandler(this.Minimize_MouseLeave);
+            // 
+            // Maximize
+            // 
+            this.Maximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Maximize.AutoSize = true;
+            this.Maximize.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Maximize.ForeColor = System.Drawing.Color.White;
+            this.Maximize.Location = new System.Drawing.Point(1415, 0);
+            this.Maximize.Name = "Maximize";
+            this.Maximize.Size = new System.Drawing.Size(48, 42);
+            this.Maximize.TabIndex = 4;
+            this.Maximize.Text = "🗗";
+            this.Maximize.Click += new System.EventHandler(this.Maximize_Click);
+            this.Maximize.MouseEnter += new System.EventHandler(this.Maximize_MouseEnter);
+            this.Maximize.MouseLeave += new System.EventHandler(this.Maximize_MouseLeave);
+            // 
+            // Close
+            // 
+            this.Close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Close.AutoSize = true;
+            this.Close.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Close.ForeColor = System.Drawing.Color.White;
+            this.Close.Location = new System.Drawing.Point(1469, 0);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(48, 42);
+            this.Close.TabIndex = 3;
+            this.Close.Text = "🗙";
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            this.Close.MouseEnter += new System.EventHandler(this.Close_MouseEnter);
+            this.Close.MouseLeave += new System.EventHandler(this.Close_MouseLeave);
+            // 
+            // MouseDrag
+            // 
+            this.MouseDrag.Fixed = true;
+            this.MouseDrag.Horizontal = true;
+            this.MouseDrag.TargetControl = this.Header;
+            this.MouseDrag.Vertical = true;
+            // 
+            // bunifuPictureBox1
+            // 
+            this.bunifuPictureBox1.AllowFocused = false;
+            this.bunifuPictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.bunifuPictureBox1.AutoSizeHeight = true;
+            this.bunifuPictureBox1.BorderRadius = 0;
+            this.bunifuPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuPictureBox1.Image")));
+            this.bunifuPictureBox1.IsCircle = false;
+            this.bunifuPictureBox1.Location = new System.Drawing.Point(116, 46);
+            this.bunifuPictureBox1.Name = "bunifuPictureBox1";
+            this.bunifuPictureBox1.Size = new System.Drawing.Size(171, 171);
+            this.bunifuPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuPictureBox1.TabIndex = 0;
+            this.bunifuPictureBox1.TabStop = false;
+            this.bunifuPictureBox1.Type = Bunifu.UI.WinForms.BunifuPictureBox.Types.Square;
+            // 
             // MainScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1520, 880);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.SidePanel);
+            this.Controls.Add(this.Header);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.SidePanel.ResumeLayout(false);
+            this.SidePanel.PerformLayout();
+            this.Header.ResumeLayout(false);
+            this.Header.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuPictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -96,10 +171,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel SidePanel;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private Bunifu.UI.WinForms.BunifuPictureBox bunifuPictureBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel Header;
+        private Bunifu.Framework.UI.BunifuDragControl MouseDrag;
+        private System.Windows.Forms.Label Close;
+        private System.Windows.Forms.Label Minimize;
+        private System.Windows.Forms.Label Maximize;
+        private Bunifu.UI.WinForms.BunifuPictureBox bunifuPictureBox1;
     }
 }
 
--- MainScreen.cs
+++ MainScreen.cs
@@ -12,9 +12,64 @@ namespace Harmony_Player
 {
     public partial class MainScreen : Form
     {
+
         public MainScreen()
         {
             InitializeComponent();
         }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void Maximize_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+            {
+                MaximizedBounds = Screen.FromHandle(Handle).WorkingArea;
+                WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                WindowState = FormWindowState.Normal;
+            }
+        }
+
+
+        private void Minimize_Click(object sender, EventArgs e)
+        {
+            WindowState= FormWindowState.Minimized;
+        }
+
+        private void Close_MouseEnter(object sender, EventArgs e)
+        {
+            Close.BackColor = Color.Red;
+        }
+
+        private void Close_MouseLeave(object sender, EventArgs e)
+        {
+            Close.BackColor = Color.Black;
+        }
+
+        private void Maximize_MouseEnter(object sender, EventArgs e)
+        {
+            Maximize.BackColor = Color.Gray;
+        }
+
+        private void Maximize_MouseLeave(object sender, EventArgs e)
+        {
+            Maximize.BackColor = Color.Black;
+        }
+
+        private void Minimize_MouseEnter(object sender, EventArgs e)
+        {
+            Minimize.BackColor = Color.Gray;
+        }
+
+        private void Minimize_MouseLeave(object sender, EventArgs e)
+        {
+            Minimize.BackColor = Color.Black;
+        }
+
     }
 }
